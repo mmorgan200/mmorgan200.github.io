23775b17473a282ed4915a1556d65985b1e833a6
Michael Morgan <mmorgan200@michael-morgans-imac.local>
2018-04-23 15:40:13 -0400

    ACPD update 23Apr2018

--- ALL_CASH_PROPERTY_DEALS/How_It_Works_files/How_It_Works.js
+++ ALL_CASH_PROPERTY_DEALS/How_It_Works_files/How_It_Works.js
@@ -4,6 +4,6 @@ setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('How_It_Works_files/How_It_WorksMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id3');performPostEffectsFixups()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id3');performPostEffectsFixups()}
 function onPageUnload()
 {Widget.onunload();}
